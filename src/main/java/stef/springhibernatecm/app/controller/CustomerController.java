37d9d22b49b1df570a25db13b41c7981eddffe6a
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-07-17 14:42:10 +0200

    Connect controller to DAO to display basic customer list on server

--- src/main/java/stef/springhibernatecm/app/controller/CustomerController.java
+++ src/main/java/stef/springhibernatecm/app/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package stef.springhibernatecm.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import stef.springhibernatecm.app.dao.ICustomerDAO;
+import stef.springhibernatecm.app.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	//inject the customerDAO
+	@Autowired
+	private ICustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		
+		//get the customers from dao
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		//add the customers to the model
+		model.addAttribute("customers", customers);
+		
 		return "customers-list";
 	}
 	
--- src/main/webapp/WEB-INF/view/customers-list.jsp
+++ src/main/webapp/WEB-INF/view/customers-list.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 
 <html>
@@ -6,7 +8,34 @@
 </head>
 
 <body>
-List of customers - coming soon...
+<div id="wrapper">
+		<div id="header">
+			<h2> CRM - Customer Relationship Manager</h2>
+		</div>
+	</div>
+	
+	<div id="container">
+		<div id="content">
+		<!-- add html table here -->
+			<table>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+				</tr>
+				
+				<c:forEach var="tempCustomer" items="${customers}">
+					<tr>
+						<th>${tempCustomer.firstName}</th>
+						<th>${tempCustomer.lastName}</th>
+						<th>${tempCustomer.email}</th>
+					</tr>
+				
+				</c:forEach>
+				
+			</table>
+		</div>
+	</div>
 </body>
 
 </html>
